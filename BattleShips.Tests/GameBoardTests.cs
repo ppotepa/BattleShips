8ce2e862ad2fe1ca721909bc014a1816c1bf64e9
Pawel.Potepa <pawel.potepa@hotmail.com>
2023-01-16 23:53:28 +0100

    Add Basic Test Fixture for Internal Class GameBoard

--- BattleShips.Tests/GameBoardTests.cs
+++ BattleShips.Tests/GameBoardTests.cs
@@ -1,4 +1,7 @@
+using BattleShips.Builders;
+using BattleShips.Exceptions;
 using BattleShips.Game;
+using BattleShips.Ships;
 using NUnit.Framework;
 
 namespace BattleShips.Tests
@@ -10,7 +13,26 @@ namespace BattleShips.Tests
         [Test]
         public void Initializing_GameBoard_With_Two_Ships_Returns_Count_Of_Two_Ships()
         {
-            Assert.Fail();
+            GameBoard gameBoard = new GameBoard
+            {
+                Options = new()
+                {
+                    GridSize = 10,
+                    MaxNumberOfBattleShips = 1,
+                    MaxNumberOfDestroyers = 1,
+                    Players =
+                    {
+                        new Player("Player 1")
+                    }
+                }
+            };
+
+            gameBoard.Initialize();
+            Assert.True(
+                gameBoard.Ships.Count is 2 &&
+                gameBoard.Ships.Any(ship => ship.GetType() == typeof(BattleShip)) &&
+                gameBoard.Ships.Any(ship => ship.GetType() == typeof(Destroyer))
+            );
         }
 
         [SetUp]
